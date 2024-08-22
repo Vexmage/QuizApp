3a8cc3f9a5c9615619d9015fd1a099a755ac3625
vextechmage <vextechmage@gmail.com>
2024-08-21 18:59:39 -0700

    Add explanations

--- Models/Question.cs
+++ Models/Question.cs
@@ -2,9 +2,11 @@
 {
     public class Question
     {
-        public int Id { get; set; } // The Id property for the question
+        public int Id { get; set; }
         public string Text { get; set; } = string.Empty;
         public List<string> Options { get; set; } = new List<string>();
         public int CorrectOptionIndex { get; set; }
+        public string? Explanation { get; set; } // Add the Explanation property
     }
+
 }
