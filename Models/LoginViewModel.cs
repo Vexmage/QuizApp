629da7bff51e8e7b3ba1a4d9de078e247a9c9ff5
vextechmage <vextechmage@gmail.com>
2024-08-23 00:55:55 -0700

    Attempt 2 GitHub Actions CI/CD workflow

--- Models/LoginViewModel.cs
+++ Models/LoginViewModel.cs
@@ -4,12 +4,12 @@ public class LoginViewModel
 {
     [Required]
     [Display(Name = "Username")]
-    public string Username { get; set; }
+    public required string Username { get; set; }
 
     [Required]
     [DataType(DataType.Password)]
     [Display(Name = "Password")]
-    public string Password { get; set; }
+    public requiredstring Password { get; set; }
 
     [Display(Name = "Remember me?")]
     public bool RememberMe { get; set; }
--- Models/RegisterViewModel.cs
+++ Models/RegisterViewModel.cs
@@ -4,20 +4,20 @@ public class RegisterViewModel
 {
     [Required]
     [Display(Name = "Username")]
-    public string Username { get; set; }
+    public required string Username { get; set; }
 
     [Required]
     [EmailAddress]
     [Display(Name = "Email")]
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 
     [Required]
     [DataType(DataType.Password)]
     [Display(Name = "Password")]
-    public string Password { get; set; }
+    public required string Password { get; set; }
 
     [DataType(DataType.Password)]
     [Display(Name = "Confirm password")]
     [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-    public string ConfirmPassword { get; set; }
+    public required string ConfirmPassword { get; set; }
 }
